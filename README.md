69c9cc9df9d62c5eb40a2c589f046738ecc1a397
primidac <primidac@gmail.com>
2020-10-31 12:39:16 +0100

    recent update

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # swiftpass
 PyQt5 Password generator with real time account tracking
+
+## What is swiftpass
+Swiftpass is a simple password generator and manager that helps you keep track of your passwords securely and keeps tracks of each password tagged to a particular account. Let's face it, we all forget our password from time to time and with multiple accounts, keeping tracks of passwords can be a headache but swiftpass simplifies everything for you and only make you to remeber only a master password to access all your passwords. swiftpass is highly secured so you don't need to worry about intruders getting hold of your password.
+
+## Features of Swiftpass
+* Swiftpass Account
+* Real time account authentication
+* Well secured passwords 
+* Master passwords
+* Strong and secured password generation
+* Account tracking 
+* Swiftpass changes your password every 2weeks PS: you don't need to remember it, only your master password
+
+Author: Primidac
+
+Website: [My Website](https://primidac.tech)
+
+Telegram: [My Telegram](https://t.me/primidac)
+
+Twitter: [My Twitter](https://twitter.com/primidac)
+
+Instagram: [My Instagram](https://instagram.com/primidaccodes)
+
---
+++ main/swiftpass.py
@@ -0,0 +1,82 @@
+# Primidac Codes
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import random
+import sqlite3 as db
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(320, 250)
+        MainWindow.setMaximumSize(QtCore.QSize(320, 250))
+        MainWindow.setSizeIncrement(QtCore.QSize(320, 250))
+        MainWindow.setBaseSize(QtCore.QSize(320, 250))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(127, 10, 71, 20))
+        self.label.setStyleSheet("font: 18 10pt \"Ubuntu Thin\";")
+        self.label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(0, 50, 320, 32))
+        self.lineEdit.setMaximumSize(QtCore.QSize(320, 250))
+        self.lineEdit.setSizeIncrement(QtCore.QSize(320, 250))
+        self.lineEdit.setBaseSize(QtCore.QSize(320, 250))
+        self.lineEdit.setStatusTip("")
+        self.lineEdit.setClearButtonEnabled(True)
+        self.lineEdit.setObjectName("lineEdit")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(0, 90, 321, 71))
+        self.textEdit.setReadOnly(True)
+        self.textEdit.setObjectName("textEdit")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(110, 160, 88, 34))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 320, 30))
+        self.menubar.setObjectName("menubar")
+        self.menuMenu = QtWidgets.QMenu(self.menubar)
+        self.menuMenu.setObjectName("menuMenu")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionCreate_Account = QtWidgets.QAction(MainWindow)
+        self.actionCreate_Account.setObjectName("actionCreate_Account")
+        self.actionLogin = QtWidgets.QAction(MainWindow)
+        self.actionLogin.setObjectName("actionLogin")
+        self.actionExit = QtWidgets.QAction(MainWindow)
+        self.actionExit.setObjectName("actionExit")
+        self.menuMenu.addAction(self.actionCreate_Account)
+        self.menuMenu.addAction(self.actionLogin)
+        self.menuMenu.addSeparator()
+        self.menuMenu.addAction(self.actionExit)
+        self.menubar.addAction(self.menuMenu.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "SwiftPass"))
+        self.label.setText(_translate("MainWindow", " Swift Pass"))
+        self.lineEdit.setToolTip(_translate("MainWindow", "Enter Name of Account for password. Eg. Facebook"))
+        self.lineEdit.setPlaceholderText(_translate("MainWindow", "Account Name"))
+        self.textEdit.setToolTip(_translate("MainWindow", "Your SwiftPass Appears Here and ge copied to your clipboard"))
+        self.textEdit.setPlaceholderText(_translate("MainWindow", "Your SwiftPass"))
+        self.pushButton.setText(_translate("MainWindow", "MySwiftPass"))
+        self.menuMenu.setTitle(_translate("MainWindow", "Menu"))
+        self.actionCreate_Account.setText(_translate("MainWindow", "Create Account"))
+        self.actionLogin.setText(_translate("MainWindow", "Login"))
+        self.actionExit.setText(_translate("MainWindow", "Exit"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
